bf266d3083ab30e8480fc5c97cbd0cff64f065bb
ASTHA <43647185+ASTHA193@users.noreply.github.com>
2019-04-06 16:00:26 +0530

    Create Functionoverloading.cpp

---
+++ Functionoverloading.cpp
@@ -0,0 +1,5 @@
+# functionoverloading
+As we know that functions are the piece of code that can be used anywhere in the program with just calling it multiple times to reduce the complexity of the code. In POP, we can use as many functions as per need, however, the names of the function shouldn’t match. In the case of OOP, we can use a function name as many times following the condition that the number of arguments or the type of arguments must differ.
+
+So the method of using the same function name for different functions is simply called function overloading. When an overloaded function is called from main program the function with matching arguments is invoked.<br/>
+[Way to the code](https://github.com/ASTHA193/functionoverloading/commit/814d431a25018d7670186490d6aa6ddbd3cdb2cb)
